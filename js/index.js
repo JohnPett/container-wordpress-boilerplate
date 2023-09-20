03159100c41a3638973b3e05c0a7e98d56ad2bbd
John Pett <john@containerstud.io>
2023-09-20 12:41:39 +0100

    Add linting

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-import { header } from './modules/header.js';
+import { header } from './modules/header.js'
 
 const setupModules = (className, include) => {
-  return [...document.getElementsByClassName(className)].map(el => {
-    const module = include(el);
-    module.init();
-    return module;
-  });
-};
-
-const initSite = () => {
-  setupModules('header', header);
+  return [...document.getElementsByClassName(className)].map((el) => {
+    const module = include(el)
+    module.init()
+    return module
+  })
 }
 
-if (document.addEventListener) document.addEventListener('DOMContentLoaded', initSite);
-else window.attachEvent('onload', initSite);
+const initSite = () => {
+  setupModules('header', header)
+}
+
+if (document.addEventListener) document.addEventListener('DOMContentLoaded', initSite)
+else window.attachEvent('onload', initSite)
