0b25e099dbfb2d68ef15f9f1e9bb91644188a375
John Pett <john@containerstud.io>
2023-09-20 18:16:47 +0100

    Add footer

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   get_header();
 ?>
 <main>
-    <h1 class="text-xl p-4 flex justify-center content-center h-screen flex-wrap">Hello world!</h1>
+    <h1 class="text-xl p-4 flex justify-center content-center h-screen flex-wrap text-green">Hello world!</h1>
 </main>
 <?php
   get_footer();
